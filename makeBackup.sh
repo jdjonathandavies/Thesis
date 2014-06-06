d03072a45106326bce05b66f7ce50827ecc3ce3b
jdjonathandavies <jdjonathandavies@googlemail.com>
2014-06-06 12:28:33 +0100

    jpd 128 pages partial detector chapter update

--- makeBackup.sh
+++ makeBackup.sh
@@ -8,4 +8,20 @@ DAY=`date +%d`
 MONTH=`date +%m`
 YEAR=`date +%y`
 
-tar --exclude='./resources/' --exclude='./otherPeoplesThesis/' --exclude=*.tar.gz -cvzf thesisBackup${YEAR}-${MONTH}-${DAY}.tar.gz ./*
+if [ -e thesisBackup${YEAR}-${MONTH}-${DAY}.tar.gz ]
+then
+    for (( backup=1 ; backup < 10; backup++ ))
+    do
+	if [ -e thesisBackup${YEAR}-${MONTH}-${DAY}.${backup}.tar.gz ]
+	then
+	    continue;
+	else
+	    tar --exclude='./resources/' --exclude='./otherPeoplesThesis/' --exclude=*.tar.gz -cvzf thesisBackup${YEAR}-${MONTH}-${DAY}.${backup}.tar.gz ./*
+	    echo "thesisBackup${YEAR}-${MONTH}-${DAY}.${backup}.tar.gz"
+	    break;
+	fi
+    done
+else
+    tar --exclude='./resources/' --exclude='./otherPeoplesThesis/' --exclude=*.tar.gz -cvzf thesisBackup${YEAR}-${MONTH}-${DAY}.tar.gz ./*
+    echo "thesisBackup${YEAR}-${MONTH}-${DAY}.tar.gz"
+fi
